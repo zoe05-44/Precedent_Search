0107a0b0233f16c22dfdb2121c8203936c57fadf
zoe05-44 <zoyafahd05@gmail.com>
2025-08-19 16:11:42 +0500

    Git and Streamlit secrets use

--- db/users_connection.py
+++ db/users_connection.py
@@ -3,7 +3,11 @@ import os
 from supabase import create_client
 import streamlit as st
 
-SUPABASE_URL = st.secrets("SUPABASE_URL")
-PUBLIC_KEY = st.secrets("PUBLIC_ROLE")
+try: 
+    SUPABASE_URL = st.secrets("SUPABASE_URL")
+    PUBLIC_KEY = st.secrets("PUBLIC_ROLE")
+except:
+    SUPABASE_URL = os.environ.get("SUPABASE_URL")
+    PUBLIC_KEY = os.environ.get("PUBLIC_ROLE")
 
 anon_supabase = create_client(SUPABASE_URL, PUBLIC_KEY)
