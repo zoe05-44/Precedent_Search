88df0a03044eb80debcd08de69a7dfd0131c839f
zoe05-44 <zoyafahd05@gmail.com>
2025-08-19 16:04:28 +0500

    edited for streamlit use

--- db/connection.py
+++ db/connection.py
@@ -1,6 +1,7 @@
 import psycopg2
+import streamlit as st
 import os
 
-DATABASE_URL = os.environ.get("DATABASE_URL")
+DATABASE_URL = st.secrets("DATABASE_URL")
 #connect to the supabase database
 conn = psycopg2.connect(DATABASE_URL)
--- db/users_connection.py
+++ db/users_connection.py
@@ -1,9 +1,9 @@
 import psycopg2
 import os
 from supabase import create_client
+import streamlit as st
 
-
-SUPABASE_URL = os.environ.get("SUPABASE_URL")
-PUBLIC_KEY = os.environ.get("PUBLIC_ROLE")
+SUPABASE_URL = st.secrets("SUPABASE_URL")
+PUBLIC_KEY = st.secrets("PUBLIC_ROLE")
 
 anon_supabase = create_client(SUPABASE_URL, PUBLIC_KEY)
